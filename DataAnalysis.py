126a380c9d1448386c89d9de49511a94b12447fd
Aquila042 <39123979+Aquila042@users.noreply.github.com>
2021-02-27 18:23:11 +0100

    Found harmonic dep

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,15 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
-from scipy.optimize import minimize 
+from scipy.optimize import minimize, curve_fit
+#from scipy.stats import linregress
 
 scan = np.loadtxt("scan.txt")
 
 #print(scan)
 
-
-#plt.figure(figsize = (10,10))
-#plt.imshow(scan, alpha=0.8, cmap='coolwarm', aspect="auto")
+bands = [433, 443, 454, 466, 478, 492, 507, 524, 540, 560, 580, 602, 627, 654,
+         682, 717, 755, 801, 856, 918, 993, 1095, 1215, 1402, 1670, 2229, 4248]
+plt.figure(figsize = (10,10))
+plt.imshow(scan, alpha=0.8, cmap='coolwarm', aspect="auto")
 
 intScan = sum(scan)#integrated scan data
 TOF = np.loadtxt("TOF.txt")
@@ -39,12 +41,12 @@ bands = [433, 443, 454, 466, 478, 492, 507, 524, 540, 560, 580, 602, 627, 654,
 m_e = 9.10953*1e-31
 Veff = 1.178185# au or 32.06 #eV
 L = 2 #m
-Hartre = 4.35974*1e-18 #J is one au energy
+Hartree = 4.35974*1e-18 #J is one au energy
 
 bandTOF = [TOF[i]*1e-9 for i in bands]
 
 def kinEnergy(gamma, tTOF):
-    return((L + gamma)**2/(2*tTOF**2) - Veff)
+    return((m_e*(L + gamma)**2/(2*tTOF**2) + Veff*Hartree)/Hartree)
 
 #bandDiff = [bands[n+1] - bands[n] for n in range(len(bands)-1)]
 
@@ -63,11 +65,81 @@ optimalGamma = minimize(bandDiff, -0.5, args=(bandTOF), tol = 1e-10).x
 print(optimalGamma, bandDiff(optimalGamma))
 #Note, as far as I can tell gamma = -0.35680721
 
+plt.figure()
 plt.plot(np.linspace(-2, 2, 1000), [bandDiff(g) for g in np.linspace(-2, 2, 1000)])
 plt.plot(optimalGamma, bandDiff(optimalGamma), "rx")
 plt.yscale("log")
 
-plt.figure(dpi=100)
-calEnergy = [kinEnergy(optimalGamma,t) for t in TOF]
-plt.plot(calEnergy, intScan)
+#plt.figure(dpi=100)
+calEnergy = [kinEnergy(optimalGamma,t) for t in TOF*1e-9]
+#plt.plot(calEnergy, intScan)
+#plt.xlim(1, 3.5)
+#plt.plot([float(calEnergy[i]) for i in bands], [intScan[i] for i in bands], "rx")
+#%%
+
+sideBands = [bands[i*2] for i in range(int(len(bands)/2 + 1))]
+
+def sideBandF(t, a, b, c, omega):
+    return(a + b*np.cos(omega*t + c))
+
+sideBandCE = np.array([float(calEnergy[i]) for i in sideBands]) #center energy of each band
+
+
+sideBandIntensity = []
+activeSB = 13
+for n in range(50):
+    sideBandIntensity.append(float((np.transpose(scan)[sideBands[activeSB] - 1][n] +
+                              np.transpose(scan)[sideBands[activeSB]][n] +
+                              np.transpose(scan)[sideBands[activeSB] + 1][n])/3))
+
+
+qIndex = [46 - 2*j for j in range(14)]
+omegaQ = 0.057*28
+
+#OBS fit needs a good p0 guess to be accurate
+
+fit = curve_fit(sideBandF, np.linspace(0,50,50), sideBandIntensity, p0 = (3, 2, 0, 0.98), bounds = [0, np.inf])
+para = fit[0]
+print(para)
+
+
+#plt.figure(dpi=100)
+#plt.plot(range(50), sideBandIntensity, ".")
+#plt.plot(np.linspace(0, 50, 500), [sideBandF(y, para[0], para[1], para[2], para[3]) for y in np.linspace(0, 50, 500)])
+
+#print(fit[0][2])
+#print(np.sqrt(np.diag(fit[1]))[2])
+#side band phases found from the above regresion
+sbPhase = [0.5839685139096004, 5.9459518118470176e-18, 2.7998216661937906e-16, 0.004831329978085194, 0.47054149214539936,
+           0.5895038166673301, 0.9239772189004821, 0.937013891261136, 1.389932618549851, 1.5449515245808427,
+           1.8557349919270725, 2.255696101490715, 2.4018044336588003, 2.6317644371219098]
+sbPhaseErr = [0.31466253656470217, 0.29384742174800565, 0.19517440210571102, 0.17750055014014526, 0.14423812602105737,
+              0.09604367666409845, 0.08313459285020776, 0.0669713560537302, 0.08729350912829566, 0.08154113637509512,
+              0.095390950932269, 0.11332368946020593, 0.19300789098949825, 0.6775244234739115]
+#the last value comes from a very weak signal
+
+def lin(x, a, b):
+    return(a + b*x)
+
+phaseDep = curve_fit(lin, sideBandCE, sbPhase, sigma = sbPhaseErr)
+linParam = phaseDep[0]
+linCov =phaseDep[1]
+
+print(linParam)
+print(np.sqrt(np.diag(linCov)))
+
+plt.figure(dpi=150)
+plt.errorbar(sideBandCE, sbPhase, sbPhaseErr, fmt = ".")
+plt.plot(np.linspace(sideBandCE[0], sideBandCE[-1]), [lin(x, linParam[0], linParam[1])
+                                 for x in np.linspace(sideBandCE[0], sideBandCE[-1])])
+plt.xlabel("Side band E_kin (au)")
+plt.ylabel("Side band Phase")
+
+#%%
+#harmonic dependence of the phase
+deltaPhase = 0.11571176477495296
+deltaPhaseErr = 0.010804025322714456
+
+
+
 
